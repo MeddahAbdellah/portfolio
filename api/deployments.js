82824ed11eb85436a64fe954c36f4dc3df5359b9
Abdallah MEDDAH <meddah.abdellah.spcx@gmail.com>
2024-02-05 18:19:08 +0100

    🐛 fix xp bg image

--- api/deployments.js
+++ api/deployments.js
@@ -1,5 +1,5 @@
-const vercelToken = process.env.VERCEL_TOKEN;
-const projectId = process.env.PORTFOLIO_PROJECT_ID;
+const vercelToken = "jeCps8a9G7XuvKkPesjAIAAP";
+const projectId = "prj_p06H9MCgjZGJUx0tAEipovMeRgS6";
 
 function enableCors(request, response, allowedDomain) {
   response.setHeader("Access-Control-Allow-Credentials", true);
