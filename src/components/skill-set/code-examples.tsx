3fb8b196267bbdb9cd203b7ea2b6b9972a61107f
Abdallah MEDDAH <meddah.abdellah.spcx@gmail.com>
2024-02-11 16:32:48 +0100

    ✨ put back Iframes

--- src/components/skill-set/code-examples.tsx
+++ src/components/skill-set/code-examples.tsx
@@ -202,7 +202,7 @@ export function CodeExamples(): React.JSX.Element {
         <></>
       )}
 
-      {/* <iframe
+      <iframe
         ref={iframeRef}
         src={url}
         onLoadCapture={() => {
@@ -212,7 +212,7 @@ export function CodeExamples(): React.JSX.Element {
           setLoading(false);
         }}
         className={`h-full rounded-lg ${loading ? " opacity-0" : ""}`}
-      ></iframe> */}
+      ></iframe>
     </main>
   );
 }
